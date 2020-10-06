55ad4911c65b8d116b2157df0aa44aac0018fe7f
Aleksi Toivonen <aptoivonen@mail.com>
2020-10-06 17:16:48 +0300

    Refactor game logic, UI

--- script.js
+++ script.js
@@ -1,85 +1,177 @@
-/* Plays 5 round game and reports winner at the end */
-function game() {
-  const CHOICES = new Map();
-  CHOICES.set("ROCK", "SCISSORS");
-  CHOICES.set("SCISSORS", "PAPER");
-  CHOICES.set("PAPER", "ROCK");
-  Object.freeze(CHOICES);
-  let gameResult = {
-    playerWins: 0,
-    computerWins: 0,
-    ties: 0,
-  };
-  const RESULTS = {
-    PLAYER_WIN: "PLAYER_WIN",
-    COMPUTER_WIN: "COMPUTER_WIN",
-    TIE: "TIE",
-  };
-  Object.freeze(RESULTS);
-
-  /* Plays one round of rps and returns string result */
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-      return RESULTS.TIE;
-    } else if (CHOICES.get(playerSelection) === computerSelection) {
-      return RESULTS.PLAYER_WIN;
-    } else {
-      return RESULTS.COMPUTER_WIN;
-    }
-  }
-
-  function updateResult(gameResult, roundResult) {
-    const newGameResult = {
-      playerWins: gameResult.playerWins,
-      computerWins: gameResult.computerWins,
-      ties: gameResult.ties,
+class GameData {
+  constructor() {
+    this.CHOICES = Object.freeze(
+      new Map()
+        .set("ROCK", "SCISSORS")
+        .set("SCISSORS", "PAPER")
+        .set("PAPER", "ROCK")
+    );
+    this.RESULTS = Object.freeze({
+      PLAYER_WIN: "PLAYER_WIN",
+      COMPUTER_WIN: "COMPUTER_WIN",
+      TIE: "TIE",
+    });
+    this.gameResult = {
+      playerWins: 0,
+      computerWins: 0,
+      ties: 0,
     };
-    if (roundResult === RESULTS.TIE) newGameResult.ties++;
-    else if (roundResult === RESULTS.PLAYER_WIN) newGameResult.playerWins++;
-    else if (roundResult === RESULTS.COMPUTER_WIN) newGameResult.computerWins++;
-    return newGameResult;
+    this.MAX_ROUNDS = 5;
+    this.round = 1;
   }
 
-  function textResult(result, playerSelection, computerSelection) {
-    if (result === RESULTS.TIE) {
-      return `Your ${playerSelection} equals computer's ${computerSelection}. A tie.`;
-    } else if (result === RESULTS.PLAYER_WIN) {
-      return `Your ${playerSelection} beats computer's ${computerSelection}! You won!`;
-    } else if (result === RESULTS.COMPUTER_WIN) {
-      return `Computer's ${computerSelection} beats your ${playerSelection}. You... have lost.`;
-    } else {
-      return "";
-    }
+  updateResult(roundResult) {
+    let { gameResult, MAX_ROUNDS, RESULTS } = this;
+    if (this.round >= MAX_ROUNDS) return;
+    if (roundResult === RESULTS.TIE) gameResult.ties++;
+    else if (roundResult === RESULTS.PLAYER_WIN) gameResult.playerWins++;
+    else if (roundResult === RESULTS.COMPUTER_WIN) gameResult.computerWins++;
+    this.round++;
   }
 
-  /* Returns random game object */
-  function computerPlay() {
-    const randomChoice = Math.floor(Math.random() * CHOICES.size);
-    const iterator = CHOICES.keys();
-    for (i = 0; i < randomChoice; i++) {
-      iterator.next();
-    }
-    return iterator.next().value;
+  isGameOver() {
+    return this.round >= this.MAX_ROUNDS;
   }
 
-  function getUserChoice(round) {
-    while (true) {
-      let userInput = prompt(`Round ${round + 1}. Give choice:`);
-      if (!userInput) return null;
-      userInput = userInput.toUpperCase();
-      if (CHOICES.get(userInput)) return userInput;
-    }
+  getFinalResult() {
+    const { gameResult, RESULTS } = this;
+    if (!this.isGameOver) return false;
+    if (gameResult.playerWins > gameResult.computerWins)
+      return RESULTS.PLAYER_WIN;
+    else if (gameResult.playerWins < gameResult.computerWins)
+      return RESULTS.COMPUTER_WIN;
+    else return RESULTS.TIE;
   }
 
-  for (let round = 0; round < 5; round++) {
-    const userChoice = getUserChoice(round);
-    const computerChoice = computerPlay();
-    if (!userChoice) break;
-    const roundResult = playRound(userChoice, computerChoice);
-    gameResult = updateResult(gameResult, roundResult);
-    console.log(textResult(roundResult, userChoice, computerChoice));
+  reset() {
+    this.gameResult = {
+      playerWins: 0,
+      computerWins: 0,
+      ties: 0,
+    };
+    this.round = 1;
   }
-  console.table(gameResult);
 }
 
-//game();
+const $ = function (id) {
+  return document.getElementById(id);
+};
+
+/* Returns random game object */
+const computerPlay = function () {
+  const { CHOICES } = gameData;
+  const randomChoice = Math.floor(Math.random() * CHOICES.size);
+  const iterator = CHOICES.keys();
+  for (i = 0; i < randomChoice; i++) {
+    iterator.next();
+  }
+  return iterator.next().value;
+};
+
+/* Plays one round of rps and returns string result */
+const playRound = function (playerSelection, computerSelection) {
+  const { RESULTS, CHOICES } = gameData;
+  if (playerSelection === computerSelection) {
+    return RESULTS.TIE;
+  } else if (CHOICES.get(playerSelection) === computerSelection) {
+    return RESULTS.PLAYER_WIN;
+  } else {
+    return RESULTS.COMPUTER_WIN;
+  }
+};
+
+const addResultClasses = function (
+  roundResult,
+  playerButtonId,
+  computerButtonId
+) {
+  const { RESULTS } = gameData;
+  if (roundResult === RESULTS.PLAYER_WIN) {
+    $(playerButtonId).classList.add("winner-btn");
+    $(computerButtonId).classList.add("loser-btn");
+  } else if (roundResult === RESULTS.COMPUTER_WIN) {
+    $(playerButtonId).classList.add("loser-btn");
+    $(computerButtonId).classList.add("winner-btn");
+  } else if (roundResult === RESULTS.TIE) {
+    $(playerButtonId).classList.add("tie-btn");
+    $(computerButtonId).classList.add("tie-btn");
+  }
+};
+
+const updateRoundLabel = function () {
+  $("round").textContent = gameData.round;
+};
+
+const updateWinLabels = function () {
+  $("player-wins").textContent = gameData.gameResult.playerWins;
+  $("computer-wins").textContent = gameData.gameResult.computerWins;
+};
+
+const showGameResult = function () {
+  if (gameData.getFinalResult() === gameData.RESULTS.PLAYER_WIN) {
+    $("winner").classList.remove("hidden");
+    $("winner").classList.add("result-player");
+  } else if (gameData.getFinalResult() === gameData.RESULTS.COMPUTER_WIN) {
+    $("winner").classList.remove("hidden");
+    $("winner").classList.add("result-computer");
+  } else if (gameData.getFinalResult() === gameData.RESULTS.TIE) {
+    $("tie").classList.remove("hidden");
+  }
+};
+
+const COMPUTER_CHOICE_ID_MAP = Object.freeze(
+  new Map()
+    .set("ROCK", "computer-rock-btn")
+    .set("PAPER", "computer-paper-btn")
+    .set("SCISSORS", "computer-scissors-btn")
+);
+
+const resolveRound = function (playerSelection, playerButtonId) {
+  const computerSelection = computerPlay();
+  const roundResult = playRound(playerSelection, computerSelection);
+  gameData.updateResult(roundResult);
+  updateRoundLabel();
+  updateWinLabels();
+  const computerButtonId = COMPUTER_CHOICE_ID_MAP.get(computerSelection);
+  addResultClasses(roundResult, playerButtonId, computerButtonId);
+  if (gameData.isGameOver()) {
+    showGameResult();
+  }
+};
+
+const playerClick = function (event) {
+  if (gameData.isGameOver()) return;
+  let userChoise = PLAYER_ID_CHOICE_MAP.get(event.currentTarget.id);
+  if (userChoise) {
+    resolveRound(userChoise, event.currentTarget.id);
+  }
+};
+
+const startNewGame = function () {
+  gameData.reset();
+  updateRoundLabel();
+  updateWinLabels();
+  $("winner").classList.add("hidden");
+  $("winner").classList.remove("result-player");
+  $("winner").classList.remove("result-computer");
+  $("tie").classList.add("hidden");
+};
+
+const PLAYER_ID_CHOICE_MAP = Object.freeze(
+  new Map()
+    .set("player-rock-btn", "ROCK")
+    .set("player-paper-btn", "PAPER")
+    .set("player-scissors-btn", "SCISSORS")
+);
+
+const playArea = $("play-area");
+playArea.addEventListener("transitionend", (event) => {
+  event.target.classList.remove("winner-btn", "loser-btn", "tie-btn");
+});
+
+$("player-rock-btn").addEventListener("click", playerClick);
+$("player-paper-btn").addEventListener("click", playerClick);
+$("player-scissors-btn").addEventListener("click", playerClick);
+$("new-game-btn").addEventListener("click", startNewGame);
+
+const gameData = new GameData();
